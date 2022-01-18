73d2b4281abb6f5abd3dcab0951594647b2e80a7
Niklas Andersson <anderssonniklas@outlook.com>
2022-01-18 19:12:53 +0100

    - Removed commented out functions

--- Solution/Raytracer/Materials.h
+++ Solution/Raytracer/Materials.h
@@ -5,9 +5,4 @@ namespace Materials
 {
 	bool ScatterRay(const Ray& aRayIn, const RayHit& aHitRecord, Vector3f& anAttenuation, Ray& aScatteredRay);
 	Vector3f EmitFromHit(const RayHit& aHitRecord);
-
-	//bool ScatterLambertian(const Ray& aRayIn, const RayHit& aHitRecord, Vector3f& anAttenuation, Ray& aScatteredRay);
-	//bool ScatterMetal(const Ray& aRayIn, const RayHit& aHitRecord, Vector3f& anAttenuation, Ray& aScatteredRay);
-	//bool ScatterDialectric(const Ray& aRayIn, const RayHit& aHitRecord, Vector3f& anAttenuation, Ray& aScatteredRay);
-	//bool ScatterLightSource(const Ray& aRayIn, const RayHit& aHitRecord);
 }
