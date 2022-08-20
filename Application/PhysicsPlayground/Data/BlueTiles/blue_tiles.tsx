529d99e0672076665c3351e8b850b0777cdd2666
Niklas Andersson <anderssonniklas@outlook.com>
2022-08-20 17:39:51 +0200

    - Added the first Tiled-level, tilesheet and tile-images

---
+++ Application/PhysicsPlayground/Data/BlueTiles/blue_tiles.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="blue_tiles" tilewidth="64" tileheight="64" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="50" source="tileBlue_01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="50" source="tileBlue_02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="tileBlue_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="tileBlue_04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="tileBlue_05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="tileBlue_06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="tileBlue_07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="tileBlue_08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="tileBlue_09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="tileBlue_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="tileBlue_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="tileBlue_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="tileBlue_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="tileBlue_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="tileBlue_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="tileBlue_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="tileBlue_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="tileBlue_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="tileBlue_19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="tileBlue_20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="tileBlue_21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="tileBlue_22.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="tileBlue_23.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="tileBlue_24.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="tileBlue_25.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="50" source="tileBlue_26.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="50" source="tileBlue_27.png"/>
+ </tile>
+</tileset>
