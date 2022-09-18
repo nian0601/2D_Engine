1d15b8117729d2ca356c8fefcba7dbf657ab46a1
Niklas Andersson <anderssonniklas@outlook.com>
2022-09-18 22:18:30 +0200

    - Reorganized all Spareparty-assets into a more reasonable filestructure, loading of everything will be broken if you sync at this specific CL

---
+++ Application/PhysicsPlayground/Data/Tilesets/Blue/BlueTiles.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="BlueTiles" tilewidth="64" tileheight="64" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="50" source="tile_01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="50" source="tile_02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="tile_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="tile_04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="tile_05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="tile_06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="tile_07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="tile_08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="tile_09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="tile_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="tile_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="tile_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="tile_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="tile_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="tile_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="tile_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="tile_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="tile_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="tile_19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="tile_20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="tile_21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="tile_22.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="tile_23.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="tile_24.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="tile_25.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="50" source="tile_26.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="50" source="tile_27.png"/>
+ </tile>
+</tileset>
---
+++ Application/PhysicsPlayground/Data/Tilesets/Specials/Specials.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Specials" tilewidth="59" tileheight="61" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="15" height="61" source="flagGreen_down.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="IsGoalPosition" type="bool" value="true"/>
+  </properties>
+  <image width="59" height="61" source="flagGreen_up.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="IsSpawnPosition" type="bool" value="true"/>
+  </properties>
+  <image width="45" height="54" source="playerBlue_stand.png"/>
+ </tile>
+</tileset>
